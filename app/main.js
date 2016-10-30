76db24868a6106602adbd8dbdc50d81d39e8c1c6
seanmtracey <seanmtracey@gmail.com>
2016-10-30 22:46:10 +0000

    Add bindings for copy/paste functionality

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 
 let win;
 
@@ -13,11 +14,31 @@ function createWindow () {
 
 	win.loadURL(`file://${__dirname}/index.html`);
 
-	//win.webContents.openDevTools();
+	// win.webContents.openDevTools();
 
 	win.on('closed', () => {
 		win = null
 	});
+
+	var template = [{
+		label: "Application",
+		submenu: [
+			{ label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+		]}, {
+		label: "Edit",
+		submenu: [
+			{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+			{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+			{ type: "separator" },
+			{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+			{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+			{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+			{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+		]}
+	];
+
+	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
 }
 
 app.on('ready', createWindow)
